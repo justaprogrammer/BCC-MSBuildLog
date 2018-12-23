bc9468a843f47e62328b5394c8a364bbf28384ad
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-12-23 16:48:43 -0500

    refactor: Removing deployment functionality from build scripts

--- Build.fsx
+++ Build.fsx
@@ -8,14 +8,11 @@ open Fake.DotNet
 open Fake.DotNet.NuGet
 open Fake.Core
 open Fake.Tools
-open Fake.Api
-open Fake.Windows
 
 BuildServer.install [
     AppVeyor.Installer
 ]
 
-let isAppveyor = AppVeyor.detect()
 let gitVersion = GitVersion.generateProperties id
 
 Target.create "Clean" (fun _ ->
@@ -105,104 +102,6 @@ Target.create "Coverage" (fun _ ->
         )
 )
 
-Target.create "DeployGitHub" (fun _ -> 
-    let gitHubToken = Environment.environVarOrNone("GITHUB_TOKEN")
-    if(gitHubToken.IsNone) then
-        Trace.traceError "GITHUB_TOKEN is not defined"
-    else
-        let (gitOwner, gitName) =
-            AppVeyor.Environment.RepoName.Split('/')
-            |> Array.pairwise
-            |> Array.head
-
-        let repoTagName = AppVeyor.Environment.RepoTagName
-        let projectName = AppVeyor.Environment.ProjectName
-
-        GitHub.createClientWithToken gitHubToken.Value
-        |> (fun clientAsync -> 
-            async {
-                let! client = clientAsync
-                let releaseClient = client.Repository.Release
-                
-                let! someRelease = async {
-                    let! exc = Async.Catch(async {
-                        let! str = Async.AwaitTask (releaseClient.Get(gitOwner, gitName, repoTagName))
-                        return str })
-
-                   match exc with      
-                   | Choice1Of2 r -> return Some r
-                   | Choice2Of2 _ -> return None
-                }
-
-                match someRelease with 
-                | Some release -> Trace.traceErrorfn "Release '%s' @ '%s' already exists" release.Name repoTagName
-                | _ ->
-                    let isPrerelease = not(String.isNullOrWhiteSpace gitVersion.PreReleaseTag)
-                    let releaseName = sprintf "%s - v%s" projectName gitVersion.SemVer
-                    let releaseBody = sprintf "## %s" releaseName
-
-                    let newRelease = new Octokit.NewRelease(repoTagName);
-                    newRelease.Name <- releaseName
-                    newRelease.Body <- releaseBody
-                    newRelease.Draft <- true
-                    newRelease.Prerelease <- isPrerelease
-
-                    let! release = releaseClient.Create(gitOwner, gitName, newRelease) |> Async.AwaitTask
-                
-                    let release : GitHub.Release = {
-                        Client = client;
-                        Owner = gitOwner;
-                        RepoName = gitName;
-                        Release = release
-                    }
-            
-                    let files = !! "nuget/*.nupkg"
-
-                    release
-                    |> async.Return
-                    |> GitHub.uploadFiles files
-                    |> GitHub.publishDraft
-                    |> Async.RunSynchronously
-
-                    Trace.traceImportantfn "Created Release: '%s' @ '%s'" releaseName repoTagName
-            }
-        )
-        |> Async.Catch
-        |> Async.RunSynchronously
-        |> ignore
-)
-
-Target.create "DeployNuGet" (fun _ -> 
-    let nugetApiKey = Environment.environVarOrNone("NUGET_API_KEY")
-    if (nugetApiKey.IsNone) then
-        Trace.traceError "NUGET_API_KEY is not defined"
-    else
-        try
-            NuGet.NuGetPublish (fun p -> { p with
-                                             AccessKey = nugetApiKey.Value
-                                             Project = "BCC-MSBuildLog"
-                                             Version = gitVersion.NuGetVersionV2
-                                             WorkingDir = "nuget" })
-                                
-            Trace.traceImportant "Uploaded NuGet Package"
-        with ex ->
-            Trace.traceError "Unable to create NuGet Package"
-            Trace.traceException ex
-)
-
-Target.create "DeployChocolatey" (fun _ -> 
-    let chocoApiKey = Environment.environVarOrNone("CHOCO_API_KEY")
-    if (chocoApiKey.IsNone) then
-        Trace.traceError "CHOCO_API_KEY is not defined"
-    else
-        try
-            !! "nuget/*.nupkg"
-            |> Seq.iter (Choco.push (fun p -> { p with ApiKey = chocoApiKey.Value }))
-        with ex ->
-            Trace.traceError "Unable to create Chocolatey Package"
-            Trace.traceException ex
-)
-
 Target.create "Default" (fun _ -> ())
 
 open Fake.Core.TargetOperators
@@ -211,12 +110,6 @@ open Fake.Core.TargetOperators
 "Build" ==> "Package" ==> "Default"
 "Build" ==> "Test" ==> "Default"
 "Build" ==> "Coverage" ==> "Default"
-
-let shouldDeploy = isAppveyor && AppVeyor.Environment.RepoTag
-
-"Package" =?> ("DeployGitHub", (shouldDeploy)) ==> "Default"
-"Package" =?> ("DeployNuGet", (shouldDeploy)) ==> "Default"
-"Package" =?> ("DeployChocolatey", (shouldDeploy)) ==> "Default"
 
 // start build
 Target.runOrDefault "Default"
